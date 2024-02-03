554c922c64d4d2e362d32909ccafc246ffbab4bb
Theodor Moroianu <theodor.moroianu@gmail.com>
2024-02-04 00:55:22 +0200

    Update

--- AdventOfCode/2021/day1part1.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-data = [int(x) for x in sys.stdin.readlines()]
-
-nr = len([i for i in range(1, len(data)) if data[i] > data[i - 1]])
-
-print(nr)
--- AdventOfCode/2021/day1part2.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-data = [int(x) for x in sys.stdin.readlines()]
-
-nr = len([i for i in range(3, len(data)) if data[i] > data[i - 3]])
-
-print(nr)
